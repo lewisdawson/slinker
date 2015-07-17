cba81895ad966d9c8ce4ec0e470e2777d163f303
Lew Dawson <lew.dawson@gmail.com>
2015-07-17 00:08:44 -0700

    Added test that checks that no symlinks were created.

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,9 +2,49 @@
 
 var should = require('chai').should(),
 	expect = require('chai').expect,
-	linker = require('../index');
+	linker = require('../index'),
+	fs = require('fs'),
+	path = require('path');
 
 describe('indexTest', function() {
+
+	var symlinkModules,
+		noSymlinkModules,
+		nodeModulePath,
+		symlinkPrefix;
+
+	symlinkModules = ['module_one', 'module_three'];
+	noSymlinkModules = ['module_two'];
+	nodeModulePath = './mock_node_modules';
+	symlinkPrefix = '@';
+
+	function constructNodeModuleSymlinkPath(module) {
+		return path.join(nodeModulePath, symlinkPrefix + module);
+	}
+
+	/**
+	 * Asserts that all symlinkModules have a symlink that exists.
+	 */
+	function assertSymlinksExist(modules) {
+		var i;
+
+		for(i = 0; i < modules.length; i++) {
+			expect(fs.existsSync(modules[i])).to.be.true;
+		}
+	}
+
+	/**
+	 * Asserts that no symlinks exist in the nodeModulePath.
+	 */
+	function assertNoSymLinksExist(modules) {
+		var exists,
+			i;
+
+		for(i = 0; i < modules.length; i++) {
+			exists = fs.existsSync(constructNodeModuleSymlinkPath(modules[i]));
+			expect(exists).to.be.false;
+		}
+	}
 
 
 	beforeEach(function() {
@@ -16,7 +56,7 @@ describe('indexTest', function() {
 	});
 
 	it('#link(): should add no symlinks if no directories (modules) are specified in options.modules', function() {
-		// TODO: add symlinks for module_one and module_two directory to mock_node_modules
+		assertNoSymLinksExist(symlinkModules.concat(noSymlinkModules));
 	});
 
 });
